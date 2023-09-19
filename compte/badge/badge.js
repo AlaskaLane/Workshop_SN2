f1a3c67c141c69ceed81d5eb1d376c1a39bdf25e
Armuri <nguyenarthur4@gmail.com>
2023-09-19 11:17:06 +0200

    badge

---
+++ compte/badge/badge.js
@@ -0,0 +1,62 @@
+function htmlFunction() {
+    document.getElementById('modal').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close').addEventListener('click', function(e) {
+    document.getElementById('modal').style.display = 'none'
+  })
+
+function cssFunction() {
+    document.getElementById('modal-1').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-1').addEventListener('click', function(e) {
+    document.getElementById('modal-1').style.display = 'none'
+  })
+
+function phpFunction() {
+    document.getElementById('modal-2').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-2').addEventListener('click', function(e) {
+    document.getElementById('modal-2').style.display = 'none'
+  })
+
+function infraFunction() {
+    document.getElementById('modal-3').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-3').addEventListener('click', function(e) {
+    document.getElementById('modal-3').style.display = 'none'
+  })
+
+  function phpFunction() {
+    document.getElementById('modal-2').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-2').addEventListener('click', function(e) {
+    document.getElementById('modal-2').style.display = 'none'
+  })
+
+function linuxFunction() {
+    document.getElementById('modal-4').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-4').addEventListener('click', function(e) {
+    document.getElementById('modal-4').style.display = 'none'
+  })
+
+function domotiqueFunction() {
+    document.getElementById('modal-5').style.display = 'block';
+    let myAudio = document.querySelector('#audio')
+myAudio.play()
+}
+document.getElementById('modal-close-5').addEventListener('click', function(e) {
+    document.getElementById('modal-5').style.display = 'none'
+  })
