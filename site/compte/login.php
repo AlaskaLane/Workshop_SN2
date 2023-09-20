59c47a9caf533e15263ee4a55707947e03de6111
Armuri <nguyenarthur4@gmail.com>
2023-09-20 09:40:35 +0200

    compte

--- site/compte/login.php
+++ site/compte/login.php
@@ -1,14 +1,12 @@
 <?php
-
 include "db.php";
 
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
-    $pseudo =$_POST["pseudo"];
+    $pseudo = $_POST["pseudo"];
     $password = $_POST["password"];
-    
     
     // Vérification de la connexion
     if ($conn->connect_error) {
@@ -16,14 +14,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     
     // Requête pour récupérer l'utilisateur avec le nom d'utilisateur donné
-    $sql = "SELECT * FROM compte WHERE pseudo ='$pseudo' AND password = '$password'";
+    $sql = "SELECT * FROM compte WHERE pseudo ='$pseudo'";
     $result = $conn->query($sql);
     
     if ($result->num_rows == 1) {
-        $_SESSION["pseudo"] = $pseudo; // Création de la session
-        header("Location: compte.php"); // Redirection vers la page d'accueil
+        // Utilisateur trouvé, vérifiez le mot de passe avec password_verify
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row["password"]; // Mot de passe haché stocké dans la base de données
+        
+        if (password_verify($password, $hashedPassword)) {
+            $_SESSION["pseudo"] = $pseudo; // Création de la session
+            header("Location: compte.php"); // Redirection vers la page d'accueil
+        } else {
+            echo "Mot de passe incorrect.";
+        }
     } else {
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
+        echo "Nom d'utilisateur incorrect.";
     }
     
     // Fermer la connexion à la base de données
