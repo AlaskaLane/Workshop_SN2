a33516e572fe608aa322f7d03cfc187f26273c79
Armuri <nguyenarthur4@gmail.com>
2023-09-20 09:42:30 +0200

    compte

--- site/compte/compte.php
+++ site/compte/compte.php
@@ -21,7 +21,7 @@ $pseudo = $_SESSION["pseudo"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="compte.css">
     <title>Gestion Compte</title>
 </head>
 <body>
